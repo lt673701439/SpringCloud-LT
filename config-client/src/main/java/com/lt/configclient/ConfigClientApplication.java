1eadd90546eabe8efafa87f61dbabbba559fda73
liutong <liutong@winchannel.net>
2018-05-24 14:17:36 +0800

    配置中心客户端修改为以service-id代替url访问，config repository 迁移进来

--- config-client/src/main/java/com/lt/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/lt/configclient/ConfigClientApplication.java
@@ -2,7 +2,9 @@ package com.lt.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
